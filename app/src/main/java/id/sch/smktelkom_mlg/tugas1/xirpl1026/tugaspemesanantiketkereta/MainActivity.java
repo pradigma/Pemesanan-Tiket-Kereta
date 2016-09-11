5a743f6a30bc1c4bfb83a16ca8636425ffe465c4
Pradigma Ayu Naratryka <pradigma.ayu.n@gmail.com>
2016-09-11 20:45:38 +0700

    fourth commit

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1026/tugaspemesanantiketkereta/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1026/tugaspemesanantiketkereta/MainActivity.java
@@ -5,22 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     EditText etNama, etTahun, etAsal, etTujuan, etTanggal, etPukul;
     // RadioButton rbLk,rbPr;
     RadioGroup rgStatus;
+    Spinner sppnp;
     Button bOK;
     CheckBox cbEk, cbEks, cbBs;
     TextView tvHasil;
+    String nama;
+    int tahun;
+    int usia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         etTahun = (EditText) findViewById(R.id.editTextTahun);
@@ -31,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
         /*rbLk = (RadioButton) findViewById(R.id.radioButtonLaki);
         rbPr = (RadioButton) findViewById(R.id.radioButtonPr);*/
         rgStatus = (RadioGroup) findViewById(R.id.RadioGroupJK);
+        cbEk = (CheckBox) findViewById(R.id.checkBoxEk);
+        cbEks = (CheckBox) findViewById(R.id.checkBoxEks);
+        cbBs = (CheckBox) findViewById(R.id.checkBoxBs);
+        cbEks.setOnCheckedChangeListener(this);
+        cbEk.setOnCheckedChangeListener(this);
+        cbBs.setOnCheckedChangeListener(this);
+        sppnp = (Spinner) findViewById(R.id.spinnerpnp);
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
 
         bOK.setOnClickListener(new View.OnClickListener() {
 
@@ -38,28 +56,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 doProcess();
                 doClick();
-                cbEk = (CheckBox) findViewById(R.id.checkBoxEk);
-                cbEks = (CheckBox) findViewById(R.id.checkBoxEks);
-                cbBs = (CheckBox) findViewById(R.id.checkBoxBs);
-
             }
 
         });
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
     }
 
     private void doClick() {
-        String hasil = "Hobi Anda\n";
+        String hasil = "";
+        String jsn = "";
+
+        if (cbEk.isChecked()) jsn += cbEk.getText() + "\n";
+        if (cbEks.isChecked()) jsn += cbEks.getText() + "\n";
+        if (cbBs.isChecked()) jsn += cbBs.getText() + "\n";
 
 
         if (rgStatus.getCheckedRadioButtonId() != -1) {
             RadioButton rb = (RadioButton)
                     findViewById(rgStatus.getCheckedRadioButtonId());
-            hasil += rb.getText().toString();
+            hasil = rb.getText().toString();
         }
 
         /*if(rbLk.isChecked()){
@@ -69,19 +86,21 @@ public class MainActivity extends AppCompatActivity {
             hasil = rbPr.getText().toString();
         }*/
 
-        if (hasil == null) {
+        if (hasil == "") {
             tvHasil.setText("Belum memilih jenis kelamin");
         } else {
-            tvHasil.setText("Jenis Kelamin anda: " + hasil);
+            tvHasil.setText("Nama anda : " + nama + "\nDengan umur : " + usia + "\nJenis Kelamin anda : " + hasil + "\nKelas yang anda pilih : " + "\n" + jsn +
+                    "Penumpang :" + sppnp.getSelectedItem().toString() + "\nAsal Keberangkatan : " + etAsal.getText().toString() + "\nTujuan Kota : " + etTujuan.getText().toString()
+                    + "\nWaktu Keberangkatan : " + etTanggal.getText().toString() + " " + etPukul.getText().toString() + "WIB");
         }
     }
 
     private void doProcess() {
         if (isValid()) {
 
-            String nama = etNama.getText().toString();
-            int tahun = Integer.parseInt(etTahun.getText().toString());
-            int usia = 2016 - tahun;
+            nama = etNama.getText().toString();
+            tahun = Integer.parseInt(etTahun.getText().toString());
+            usia = 2016 - tahun;
         }
     }
 
@@ -122,9 +141,12 @@ public class MainActivity extends AppCompatActivity {
             etPukul.setError("Pukul atau jam keberangkatan belum diisi");
         }
 
-
         return valid;
     }
 
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
 }
