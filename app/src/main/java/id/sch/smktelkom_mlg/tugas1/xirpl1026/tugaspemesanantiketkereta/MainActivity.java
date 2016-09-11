713aa6ca164674deeda6a5bb8b8564190ec46e39
Pradigma Ayu Naratryka <pradigma.ayu.n@gmail.com>
2016-09-11 13:38:17 +0700

    first commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1026/tugaspemesanantiketkereta/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.tugas1.xirpl1026.tugaspemesanantiketkereta;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
